408db653b895499d68da9887d3b1541b860f8282
yed_ <yed@vanyli.net>
2012-09-12 12:39:24 +0200

    fix category linking

--- yummy/urls.py
+++ yummy/urls.py
@@ -5,16 +5,16 @@ from yummy.views import CategoryView, IngredientView, RecipeDetail, CategoryReor
 
 
 urlpatterns = patterns('',
-    url(r'^%s/(?P<ingredient>[\w|-]+)/$' % _("ingredient"), IngredientView.as_view(), name='ingredient_detail'),
+    url(r'^%s/(?P<ingredient>[\w-]+)/$' % _("ingredient"), IngredientView.as_view(), name='ingredient_detail'),
     url(r'^%s/$' % _("ingredient"), IngredientView.as_view(), name='ingredient_index'), #TODO
 
-    url(r'^(?P<recipe_slug>[\w|-]+)/$', RecipeDetail.as_view(), name='recipe_detail'),
+    url(r'^(?P<recipe_slug>[\w-]+)/$', RecipeDetail.as_view(), name='recipe_detail'),
 
 
-    url(r'^category_reorder/(?P<order_attr>[\w|-]+)/$', CategoryReorder.as_view(), name='category_reorder'),
-    url(r'^category_reorder/(?P<order_attr>[\w|-]+)/(?P<photo_attr>\w+)/$', CategoryReorder.as_view(), name='category_reorder'),
+    url(r'^category_reorder/(?P<order_attr>[\w-]+)/$', CategoryReorder.as_view(), name='category_reorder'),
+    url(r'^category_reorder/(?P<order_attr>[\w-]+)/(?P<photo_attr>\w+)/$', CategoryReorder.as_view(), name='category_reorder'),
 
 
-    url(r'^%s/(?P<category_path>[\w|-]+)/$' % _("category"), CategoryView.as_view(), name='category_detail'),
+    url(r'^%s/(?P<category_path>[\w/-]+)/$' % _("category"), CategoryView.as_view(), name='category_detail'),
     url(r'^$', CategoryView.as_view(), name='category_detail'),
 )
