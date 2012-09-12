1853971834fc3c279f4e4e93a5e8466d04a10ee4
Vitek Pliska <whit@jizak.cz>
2012-09-12 17:09:24 +0200

    More units

--- yummy/conf.py
+++ yummy/conf.py
@@ -1,12 +1,20 @@
+from collections import namedtuple
 from django.conf import settings
-from django.utils.translation import ngettext, npgettext, ugettext_lazy as _
+from django.utils.translation import npgettext, ugettext_lazy as _
 
-#ngettext(singular, plural, number)
-#npgettext(context, singular, plural, number)
-
-UNIT_CHOICES = (
-    (0, npgettext('unit', 'cup', 'cups', 0)),
+Unit = namedtuple('Unit', 'id name abbr')
+UNITS = (
+    Unit(0, npgettext('unit', 'piece', 'pieces', 1), npgettext('unit', 'pc', 'pcs', 1)),
+    Unit(1, npgettext('unit', 'gram', 'grams', 1), 'g'),
+    Unit(2, npgettext('unit', 'dekagram', 'dekagrams', 1), 'dkg'),
+    Unit(3, npgettext('unit', 'kilogram', 'kilograms', 1), 'kg'),
+    Unit(4, npgettext('unit', 'mililiter', 'mililiters', 1), 'ml'),
+    Unit(5, npgettext('unit', 'deciliter', 'deciliters', 1), 'dl'),
+    Unit(6, npgettext('unit', 'liter', 'liters', 1), 'l'),
+    Unit(100, npgettext('unit', 'cup', 'cups', 1), npgettext('unit', 'cup', 'cups', 1)),
 )
+UNIT_CHOICES = tuple(unit[:2] for unit in UNITS)
+
 
 PRICING_CHOICES = (
     (1, _('Cheapest')),
@@ -32,7 +40,7 @@ RECIPE_RECOMMENDATIONS_COUNT = 3
 
 CATEGORY_ORDER_DEFAULT = getattr(settings, 'YUMMY_CATEGORY_ORDER', '-created')
 CATEGORY_ORDERING = {
-    'slug': _("by ranking"), #TODO - add ranking
+    'slug': _("by ranking"),  # TODO - add ranking
     'title': _("by alphabet"),
     '-created': _("by date"),
 }
