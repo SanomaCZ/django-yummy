cc8258728933b5e2a05b5f026b432bcae3a4439b
Vitek Pliska <whit@jizak.cz>
2012-09-14 16:34:39 +0200

    Unused context variable

--- yummy/context_processors.py
+++ yummy/context_processors.py
@@ -1,8 +1,7 @@
 from yummy.models import Category
-from django.conf import settings
+
 
 def common(request):
     return {
-        'BASE_TEMPLATE': getattr(settings, 'YUMMY_EXTENDS_TEMPLATE', 'base.html'),
         'recipes_categories': Category.objects.filter(parent__isnull=True),
     }
