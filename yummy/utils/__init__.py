6b2670f7c34a450cd6c10b0070af2c4e7e7efa57
michal dub <michal.dub@sanomamedia.cz>
2012-10-08 14:02:17 +0200

    keep compatibility with python 2.5

--- yummy/utils/__init__.py
+++ yummy/utils/__init__.py
@@ -7,7 +7,7 @@ def import_module_member(modstr):
         mod_path, attr = modstr.rsplit('.', 1)
         mod = import_module(mod_path)
         member = getattr(mod, attr)
-    except (AttributeError, ImportError, ValueError) as e:
+    except (AttributeError, ImportError, ValueError), e:
         raise ImproperlyConfigured('Error importing %s: "%s"' % (modstr, e))
     else:
         return member
