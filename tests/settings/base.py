a7b7de0138f5a03f2c770fd892210a01523be4a9
michal dub <michal.dub@sanomamedia.cz>
2016-03-18 14:04:40 +0100

    fixed test settings to work with django 1.9

--- tests/settings/base.py
+++ tests/settings/base.py
@@ -85,23 +85,10 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '%c(da62lq0cqb9k)p%%9@qdh9df^h469v8oh=33zo4$7r5%e^a'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-ROOT_URLCONF = 'tests.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-#WSGI_APPLICATION = 'example_project.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -120,6 +107,30 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     'yummy.context_processors.common',
 )
+
+TEMPLATE_OPTIONS = {
+    'context_processors': TEMPLATE_CONTEXT_PROCESSORS,
+    'loaders': TEMPLATE_LOADERS,
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'OPTIONS': TEMPLATE_OPTIONS,
+        'DIRS': TEMPLATE_DIRS,
+    },
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'tests.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+#WSGI_APPLICATION = 'example_project.wsgi.application'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
