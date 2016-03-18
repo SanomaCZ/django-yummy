aca61940bbaed958827f9d3580a480361fbc2ced
michal dub <michal.dub@sanomamedia.cz>
2016-03-18 14:05:40 +0100

    used get_model from utils

--- yummy/managers.py
+++ yummy/managers.py
@@ -1,8 +1,8 @@
 from datetime import date
 from django.core.cache import cache
 from django.db import models
-from django.db.models.loading import get_model
 
+from yummy.utils import get_model
 from yummy import conf
 
 
