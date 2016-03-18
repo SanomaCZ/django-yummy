48bd9b2c5d969f6cd07ddb3759cf4c3388df7476
michal dub <michal.dub@sanomamedia.cz>
2016-03-18 14:05:18 +0100

    fixed get_model and import_module for django 1.9

--- yummy/utils/__init__.py
+++ yummy/utils/__init__.py
@@ -1,5 +1,16 @@
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.importlib import import_module
+
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module
+
+try:
+    from django.apps import apps
+except ImportError:  # django < 1.7
+    from django.db.models.loading import get_model
+else:
+    get_model = apps.get_model
 
 
 def import_module_member(modstr):
